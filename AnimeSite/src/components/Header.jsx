62bbd03e205de27977fadf34c65f172124f15cd4
udK01 <dominik.muranyi@gmail.com>
2024-03-24 23:24:12 +0000

    Scuffed Navbar Adjustments

---
+++ AnimeSite/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import SearchBar from "./SearchBar";
+import { logo } from "../assets";
+
+export default function Header() {
+  return (
+    <div className="flex w-full bg-tertiary">
+      <div className="flex items-center justify-between px-5 py-2 w-[1200px] mx-auto">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-[50px] h-[50px] object-contain"
+        />
+        <SearchBar />
+      </div>
+    </div>
+  );
+}
--- AnimeSite/src/components/Navbar.jsx
+++ AnimeSite/src/components/Navbar.jsx
@@ -1,32 +1,31 @@
 import { navLinks } from "../constants";
-import SearchBar from "./SearchBar";
-import { logo } from "../assets";
+import Header from "./Header";
 
 export default function Navbar() {
   return (
     <>
-      <div className="bg-tertiary w-full">
-        <img
-          src={logo}
-          alt="logo"
-          className="w-[50px] h-[50px] object-contain"
-        />
-      </div>
-      <nav className="mx-auto p-2 w-full bg-primary">
-        <div className="flex justify-center font-poppins text-white">
-          {/* Logo */}
-          {/* Add your logo component here */}
-
-          {/* Navigation links */}
-          {navLinks.map((link, index) => (
-            <div
-              key={link.id}
-              className={`${index !== navLinks.length - 1 ? "mr-5" : "mr-0"}`}
-            >
-              {link.title}
+      <Header />
+      <nav className="flex w-full bg-primary">
+        <div className="flex justify-between mx-auto w-[1150px] font-poppins text-white">
+          <div className="flex">
+            {navLinks.slice(0, -1).map((link, index) => (
+              <div
+                key={link.id}
+                className={`${
+                  index !== navLinks.length - 2 ? "mr-5" : "mr-0"
+                } hover:bg-secondary p-2`}
+              >
+                {link.title}
+              </div>
+            ))}
+          </div>
+          {navLinks.length > 0 && (
+            <div>
+              <div className="mr-0 hover:bg-secondary p-2">
+                {navLinks[navLinks.length - 1].title}
+              </div>
             </div>
-          ))}
-          <SearchBar />
+          )}
         </div>
       </nav>
     </>
