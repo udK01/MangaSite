1a8371f1292c7edbb07bc7114a285e4ceefb5eb7
udK01 <dominik.muranyi@gmail.com>
2024-03-24 23:05:32 +0000

    Added logo asset

--- AnimeSite/src/components/Navbar.jsx
+++ AnimeSite/src/components/Navbar.jsx
@@ -1,26 +1,34 @@
 import { navLinks } from "../constants";
 import SearchBar from "./SearchBar";
+import { logo } from "../assets";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between w-full p-2 bg-primary">
-      <div className="flex items-center font-poppins text-white">
-        {/* Logo */}
-        {/* Add your logo component here */}
-
-        {/* Navigation links */}
-        {navLinks.map((link, index) => (
-          <div
-            key={link.id}
-            className={`${index !== navLinks.length - 1 ? "mr-5" : "mr-0"}`}
-          >
-            {link.title}
-          </div>
-        ))}
+    <>
+      <div className="bg-tertiary w-full">
+        <img
+          src={logo}
+          alt="logo"
+          className="w-[50px] h-[50px] object-contain"
+        />
       </div>
+      <nav className="mx-auto p-2 w-full bg-primary">
+        <div className="flex justify-center font-poppins text-white">
+          {/* Logo */}
+          {/* Add your logo component here */}
 
-      {/* SearchBar */}
-      <SearchBar />
-    </nav>
+          {/* Navigation links */}
+          {navLinks.map((link, index) => (
+            <div
+              key={link.id}
+              className={`${index !== navLinks.length - 1 ? "mr-5" : "mr-0"}`}
+            >
+              {link.title}
+            </div>
+          ))}
+          <SearchBar />
+        </div>
+      </nav>
+    </>
   );
 }
--- AnimeSite/tailwind.config.js
+++ AnimeSite/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       colors: {
         primary: "#983ce4",
         secondary: "#18141c",
+        tertiary: "#100c0c",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
       },
