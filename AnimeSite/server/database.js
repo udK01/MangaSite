26320964d524153a5e35e9eed985b0e9503c2e64
udK01 <dominik.muranyi@gmail.com>
2024-03-30 11:09:22 +0000

    Initialised Database/Server Files

---
+++ AnimeSite/server/database.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const db = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  })
+  .promise();
---
+++ AnimeSite/server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {} from "./database.js";
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ error: "Internal server error. Please try again later." });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}.`);
+});
