f8a130a031ea4af23f15e17c7d506f1f394b4a0c
udK01 <dominik.muranyi@gmail.com>
2024-03-30 16:24:13 +0000

    Added user creation with simple error handling.

--- AnimeSite/server/database.js
+++ AnimeSite/server/database.js
@@ -11,3 +11,40 @@ const db = mysql
     database: process.env.DB_NAME,
   })
   .promise();
+
+// Users
+
+async function createUser(username, password, accessLevel = 0) {
+  try {
+    // Temporary "Default" Profile Picture.
+    const defaultProfilePicture = "../public/vite.svg";
+
+    // Sql Query.
+    const query = `
+      INSERT INTO Users (Username, Password, Profile_Picture, Bookmarks, Access_Level)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+
+    // Values To Enter.
+    const values = [
+      username,
+      password,
+      defaultProfilePicture,
+      JSON.stringify([]),
+      accessLevel,
+    ];
+
+    // Add & Feedback.
+    await db.query(query, values);
+    console.log(`Created user: ${username}`);
+  } catch (error) {
+    // Error Handling For Duplicate Usernames.
+    if (error.code === "ER_DUP_ENTRY") {
+      console.error(`Username "${username}" already exists.`);
+    } else {
+      console.error("Failed to create new user:", error);
+    }
+  }
+}
+
+await createUser(`Steve`, `password`, `0`);
