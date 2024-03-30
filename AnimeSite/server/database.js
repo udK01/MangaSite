6333af9deead8a81b87d330a0edd2fda69c77f49
udK01 <dominik.muranyi@gmail.com>
2024-03-30 18:02:01 +0000

    Added bookmark handling function and create for mangas.

--- AnimeSite/server/database.js
+++ AnimeSite/server/database.js
@@ -14,11 +14,14 @@ const db = mysql
   })
   .promise();
 
-// Users
+/////////
+//Users//
+/////////
+
 /**
  *
  * Creates new users with the inputted username and password,
- * default profile picture, empty bookmarks and lowest access level.
+ * default profile picture and lowest access level.
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Admittedly, I will probably refactor it. Make a single admin account
@@ -29,9 +32,14 @@ const db = mysql
  *            1 - Moderator
  *            2 - Admin
  *
+ * Example Usage.
+ *
+ * await createUser("username", "password");
+ *
  * @param {string} username The username given by the user.
  * @param {string} password The password given by the user.
  * @param {integer} accessLevel The level of authority the user is granted.
+ *
  */
 export async function createUser(username, password, accessLevel = 0) {
   try {
@@ -40,18 +48,12 @@ export async function createUser(username, password, accessLevel = 0) {
 
     // Sql Query.
     const query = `
-      INSERT INTO Users (Username, Password, Profile_Picture, Bookmarks, Access_Level)
-      VALUES (?, ?, ?, ?, ?)
+      INSERT INTO Users (username, password, profilePicture, accessLevel)
+      VALUES (?, ?, ?, ?)
     `;
 
     // Values To Enter.
-    const values = [
-      username,
-      password,
-      defaultProfilePicture,
-      JSON.stringify([]),
-      accessLevel,
-    ];
+    const values = [username, password, defaultProfilePicture, accessLevel];
 
     // Add & Feedback.
     await db.query(query, values);
@@ -69,7 +71,12 @@ export async function createUser(username, password, accessLevel = 0) {
  *
  * Deletes the user with the specified username.
  *
+ * Example Usage.
+ *
+ * await deleteUser("udk");
+ *
  * @param {string} username The username of the user to be deleted.
+ *
  */
 export async function deleteUser(username) {
   try {
@@ -83,8 +90,13 @@ export async function deleteUser(username) {
  *
  * Fetches the user based on username.
  *
+ * Example Usage.
+ *
+ * await getUser("udk");
+ *
  * @param {string} username The user's username.
  * @returns the user.
+ *
  */
 export async function getUser(username) {
   try {
@@ -101,9 +113,14 @@ export async function getUser(username) {
  * Finds user, checks if they entered the
  * correct password and changes it to a new one.
  *
+ * Example Usage.
+ *
+ * await changePassword(1, "password", "password123");
+ *
  * @param {int} userID The user's unique ID.
  * @param {string} oldPassword The user's old password.
  * @param {string} newPassword The user's new password.
+ *
  */
 export async function changePassword(userID, oldPassword, newPassword) {
   try {
@@ -117,5 +134,131 @@ export async function changePassword(userID, oldPassword, newPassword) {
     }
   } catch (error) {
     console.error(`Failed to change password!`, error);
+  }
+}
+
+/**
+ *
+ * Changes the user's profile picture.
+ *
+ * @param {int} userID The user's unique ID.
+ * @param {string} profilePicture A path to the new picture.
+ *
+ * Example Usage.
+ *
+ * await changeProfilePicture(1, "...");
+ *
+ */
+export async function changeProfilePicture(userID, profilePicture) {
+  try {
+    const query = `UPDATE Users SET profilePicture = ? WHERE userID = ?`;
+    const values = [profilePicture, userID];
+    await db.query(query, values);
+  } catch (error) {
+    console.error(`Failed to change pictures:`, error);
+  }
+}
+
+/////////////
+//Bookmarks//
+/////////////
+
+/**
+ *
+ * Adds or removes a bookmark.
+ *
+ * Example Usage
+ *
+ * await alterBookmarks(1,1,"add");
+ *
+ * @param {int} userID The users's unique identifier.
+ * @param {int} mangaID The manga's unique identifier.
+ * @param {string} action The action to "add/remove".
+ *
+ */
+async function alterBookmarks(userID, mangaID, action) {
+  try {
+    switch (action) {
+      case "add":
+        await db.query(
+          `INSERT INTO user_bookmarks (userID, mangaID) VALUES (?, ?)`,
+          [userID, mangaID]
+        );
+        break;
+      case "remove":
+        await db.query(
+          `DELETE FROM user_bookmarks WHERE userID = ? AND mangaID = ?`,
+          [userID, mangaID]
+        );
+        break;
+    }
+  } catch (error) {
+    console.error(`Failed to add bookmark:`, error);
+  }
+}
+
+//////////
+//Mangas//
+//////////
+
+/**
+ *
+ * Creates a new comic with the specified title,
+ * image, type, description, author and status.
+ *
+ * Example Usage.
+ *
+ * await createComic(
+ *  `Comic 2`,
+ *  `Cat or Dogs?`,
+ *  `14`,
+ *  `Manga`,
+ *  `The sneaky cat, pushed the dull dog...`,
+ *  `FoxLover`,
+ *  `OnGoing`
+ * );
+ *
+ * @param {string} mangaTitle The comic's title.
+ * @param {string} mangaImage The comic's image.
+ * @param {int} totalChapters The comic's cumulative chapters.
+ * @param {string} type The comic's type.
+ * @param {string} description The comic's description.
+ * @param {string} author The comic's author.
+ * @param {string} status The comic's status.
+ *
+ */
+async function createComic(
+  mangaTitle,
+  mangaImage,
+  totalChapters,
+  type,
+  description,
+  author,
+  status
+) {
+  try {
+    let defaultRating = 0;
+
+    // Sql Query.
+    const query = `
+      INSERT INTO Mangas (mangaTitle, mangaImage, totalChapters, rating, type, description, author, status)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+
+    // Values To Enter.
+    const values = [
+      mangaTitle,
+      mangaImage,
+      totalChapters,
+      defaultRating,
+      type,
+      description,
+      author,
+      status,
+    ];
+
+    await db.query(query, values);
+  } catch (error) {
+    console.error(`Couldn't create comic:`, error);
   }
 }
