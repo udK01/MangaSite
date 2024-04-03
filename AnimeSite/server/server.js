2151a54b4a3bca19524024b7a052eff1543b64ea
udK01 <dominik.muranyi@gmail.com>
2024-04-03 21:26:35 +0100

    Added more mangas, altered chapter.

--- AnimeSite/server/server.js
+++ AnimeSite/server/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import multer from "multer";
-import path from "path";
 import * as databaseFunctions from "./database.js";
 
 const PORT = 8080;
--- AnimeSite/src/components/Chapter.jsx
+++ AnimeSite/src/components/Chapter.jsx
@@ -3,11 +3,11 @@ export default function Chapter({ chapter }) {
     <div className="flex items-center justify-between mt-3">
       <div className="flex items-center mx-3">
         <p className="w-1 h-1 rounded-full bg-red-500" />
-        <p className="hover:text-primary hover:cursor-pointer ml-3 text-[14px]">
-          Chapter {chapter.chapterNumber}
+        <p className="hover:text-primary hover:cursor-pointer ml-3 text-[14px] line-clamp-1 text-ellipsis">
+          Chapter {chapter.chapterNumber} - {chapter.chapterTitle}
         </p>
       </div>
-      <p className="text-dimWhite text-[14px]">4 days ago</p>
+      <p className="text-dimWhite text-[14px] whitespace-nowrap">4 days ago</p>
     </div>
   );
 }
