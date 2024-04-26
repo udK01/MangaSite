986525eccbc170996b72b5f104d02cf443be9e45
udK01 <dominik.muranyi@gmail.com>
2024-04-26 18:14:03 +0100

    Created dropdown for comics, completed status search.

---
+++ AnimeSite/src/components/ComicsView/ComicDropdown.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+
+const ComicDropdown = ({ options, value, func }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [checkedBoxes, setCheckedBoxes] = useState([]);
+  const dropdownRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // Function to toggle checkbox
+  const toggleCheckbox = (genreTag) => {
+    let updatedCheckedBoxes;
+    if (checkedBoxes.includes(genreTag)) {
+      updatedCheckedBoxes = checkedBoxes.filter((tag) => tag !== genreTag);
+    } else {
+      updatedCheckedBoxes = [...checkedBoxes, genreTag];
+    }
+    setCheckedBoxes(updatedCheckedBoxes);
+    func(updatedCheckedBoxes);
+  };
+
+  // Function to render checkboxes and options
+  const renderOptions = () => {
+    const rows = [];
+    for (let i = 0; i < options.length; i += 5) {
+      const rowOptions = options.slice(i, i + 5);
+      rows.push(
+        <div key={i} className="flex items-center">
+          {rowOptions.map((option, index) => (
+            <div key={index} className="flex items-center mr-4 w-[150px]">
+              <input
+                type="checkbox"
+                id={`checkbox-${i + index}`}
+                checked={checkedBoxes.includes(option.genreTag)}
+                onChange={() => toggleCheckbox(option.genreTag)}
+                className="mr-2 ml-5"
+              />
+              <label
+                htmlFor={`checkbox-${i + index}`}
+                className="cursor-pointer hover:text-primary whitespace-nowrap overflow-hidden overflow-ellipsis"
+              >
+                {option.genreTag}
+              </label>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return rows;
+  };
+
+  return (
+    <div className="relative font-poppins" ref={dropdownRef}>
+      {/* Button */}
+      <div
+        className={`ml-[17px] flex items-center justify-evenly w-[150px] bg-quinary rounded-sm hover:bg-primary`}
+        onClick={toggleDropdown}
+      >
+        <div>{value}</div>
+        {isOpen ? <FaChevronUp /> : <FaChevronDown />}
+      </div>
+      {/* Drop Down */}
+      {isOpen && (
+        <div className="scrollbar-thumb-primary scrollbar-track-transparent">
+          <div
+            className={`w-[854px] absolute mt-1 bg-secondary text-white border-2 border-primary scrollbar-thin max-h-[200px] overflow-y-auto z-10`}
+          >
+            {renderOptions()}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ComicDropdown;
--- AnimeSite/src/components/ComicsView/Comics.jsx
+++ AnimeSite/src/components/ComicsView/Comics.jsx
@@ -1,13 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Separator from "../Separator";
+import ComicDropdown from "./ComicDropdown";
 import Card from "../PopularToday/PopularTodayCard";
 
 export default function Comics({ comics }) {
   const [mangas, setMangas] = useState(comics);
+  const [filteredMangas, setFilteredMangas] = useState(mangas);
+
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/getGenres")
+      .then((response) => setGenres(response.data[0]))
+      .catch((error) => console.error(`Failed to fetch genres:`, error));
+  }, []);
+
+  // Filter according to genres.
+  useEffect(() => {
+    setFilteredMangas(
+      mangas.filter((manga) =>
+        selectedGenres.every((genre) => manga.genres.includes(genre))
+      )
+    );
+  }, [selectedGenres]);
 
   const buttonFormatting =
-    "ml-[17px] flex justify-center w-[150px] bg-quinary rounded-md";
+    "ml-[17px] flex justify-center w-[150px] bg-quinary rounded-sm";
 
   return (
     <section className="w-[854px] h-auto bg-quaternary rounded-sm font-poppins">
@@ -16,18 +38,28 @@ export default function Comics({ comics }) {
       </div>
       <Separator />
       <div className="flex text-white mb-3">
-        <div className={buttonFormatting}>Genre All</div>
+        <ComicDropdown
+          options={genres}
+          value={"Genre All"}
+          func={setSelectedGenres}
+        />
         <div className={buttonFormatting}>Status All</div>
         <div className={buttonFormatting}>Type All</div>
         <div className={buttonFormatting}>Order by Update</div>
         <div className={buttonFormatting}>🔍 Search</div>
       </div>
-      <div className="flex flex-wrap justify-evenly pb-6">
-        {mangas.map((manga, index) => (
-          <div key={index} className="flex mt-2">
-            <Card manga={manga} />
-          </div>
-        ))}
+      <div className="flex flex-wrap pb-6">
+        {filteredMangas.length > 0 ? (
+          filteredMangas.map((manga, index) => (
+            <div key={index} className="flex mt-2 ml-5">
+              <Card manga={manga} />
+            </div>
+          ))
+        ) : (
+          <h2 className="w-full flex justify-center text-white font-poppins text-[32px] mt-5">
+            There are no comics found.
+          </h2>
+        )}
       </div>
     </section>
   );
