a7ffcf962a422406c9c10d896dfb601248e9c96d
udK01 <dominik.muranyi@gmail.com>
2024-05-01 19:38:49 +0100

    Added login UI.

--- AnimeSite/src/components/LoginRegister/Login.jsx
+++ AnimeSite/src/components/LoginRegister/Login.jsx
@@ -1,3 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import { FaAngleLeft } from "react-icons/fa";
+import { useState } from "react";
+
 export default function Login() {
-  return <section>Login.</section>;
+  const [username, setUsername] = useState("Username");
+  const [password, setPassword] = useState("Password");
+  const navigate = useNavigate();
+
+  function customInputField(type, placeholder, value, func, mt = 0) {
+    const commonProps = {
+      placeholder: placeholder,
+      value: value,
+      onChange: (e) => func(e.target.value),
+      onFocus: (e) => {
+        if (e.target.value === placeholder) {
+          func("");
+        }
+      },
+      onBlur: (e) => {
+        if (e.target.value === "") {
+          func(placeholder);
+        }
+      },
+      className: `w-[250px] min-h-[34px] px-4 mt-${mt} rounded-sm border-2 border-quaternary bg-secondary text-white hover:cursor-pointer hover:text-primary`,
+    };
+
+    return <input {...commonProps} type={type} />;
+  }
+
+  return (
+    <section className="w-[600px] h-[400px] my-[120px] flex justify-center border-2 border-primary font-poppins py-1 bg-quaternary rounded-sm">
+      <div className="flex flex-col justify-center mx-4 text-white items-center">
+        <div className="text-[20px]">Please login.</div>
+        {customInputField("text", "Username", username, setUsername, 2)}
+        {customInputField("password", "Password", password, setPassword, 2)}
+        <div className="w-[250px] mt-3 flex justify-between items-center text-white">
+          <button
+            className="flex items-center bg-quinary p-1 pr-3 rounded-md hover:bg-primary"
+            onClick={() => navigate("/register")}
+          >
+            {<FaAngleLeft />}Register
+          </button>
+          <button className="bg-primary p-1 px-2 rounded-md hover:bg-purple-700">
+            Login
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
