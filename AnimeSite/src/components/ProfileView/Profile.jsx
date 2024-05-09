ad2be32800a58c291755cabccdfd7eb3b8b9776a
udK01 <dominik.muranyi@gmail.com>
2024-05-09 18:44:35 +0100

    Extended profile with basic user info.

--- AnimeSite/src/components/ProfileView/Profile.jsx
+++ AnimeSite/src/components/ProfileView/Profile.jsx
@@ -8,35 +8,108 @@ import axios from "axios";
 export default function Profile() {
   const { user } = useContext(UserContext);
   const [profileOwner, setProfileOwner] = useState();
+  const [editing, setEditing] = useState(false);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const username = searchParams.get("user");
     axios
       .get(`/api/users/${username}`)
-      .then((response) => setProfileOwner(response.data))
+      .then((response) => setProfileOwner(response.data[0]))
       .catch((error) => console.error(`Failed to get user:`, error));
   }, [location.search]);
 
+  function handleImageChange(event) {
+    const selectedImage = event.target.files[0];
+    setImage(selectedImage);
+  }
+
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    if (files.length > 0) {
+      const selectedImage = files[0];
+      setImage(selectedImage);
+    }
+  }
+
   return (
-    <section className="w-[826px] h-aut rounded-sm font-poppins text-white">
-      <div className="flex justify-between text-[14px] w-full bg-quaternary rounded-sm text-white p-2">
-        <div className="flex">
-          <Link
-            to={"/"}
-            className="hover:text-primary mr-2 transition-colors duration-300"
-          >
-            Home
-          </Link>
-          <p className="transition-colors duration-300">&gt;</p>
-          <Link
-            to={`/profile`}
-            className="hover:text-primary mx-2 transition-colors duration-300"
-          >
-            {profileOwner && profileOwner[0].username}
-          </Link>
+    profileOwner && (
+      <section className="w-[826px] h-aut rounded-sm font-poppins text-white">
+        {/* Route */}
+        <div className="flex justify-between text-[14px] w-full bg-quaternary rounded-sm text-white p-2">
+          <div className="flex">
+            <Link
+              to={"/"}
+              className="hover:text-primary mr-2 transition-colors duration-300"
+            >
+              Home
+            </Link>
+            <p className="transition-colors duration-300">&gt;</p>
+            <Link
+              to={`/profile?user=${profileOwner.username}`}
+              className="hover:text-primary mx-2 transition-colors duration-300"
+            >
+              {profileOwner.username}
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+        {/* Body */}
+        <div className="flex w-full h-auto bg-quaternary mt-10 p-4">
+          {/* Left */}
+          <div className="flex flex-col w-[180px] flex-shrink-0">
+            {/* Image Selector */}
+            <div className="relative border-2 size-36 border-primary flex justify-center items-center bg-secondary">
+              <img
+                src={profileOwner.profilePicture}
+                alt={profileOwner.profilePicture}
+                className={`${editing && "brightness-50"} w-full h-full`}
+              />
+              {editing && (
+                <div
+                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-secondary rounded-full p-1"
+                  onDragOver={handleDragOver}
+                  onDrop={handleDrop}
+                >
+                  <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className="hidden"
+                    id="fileInput"
+                  />
+                  <label
+                    htmlFor="fileInput"
+                    className="cursor-pointer text-white hover:text-primary text-3xl"
+                  >
+                    <FaEdit />
+                  </label>
+                  {image && (
+                    <p className="absolute w-[150px] -translate-x-12 text-white mt-2">
+                      Selected Image: <br /> {image.name}
+                    </p>
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
+          {/* Right */}
+          <div className="flex flex-col">
+            <div className=" ml-4 text-white text-[32px] w-full transition-colors duration-300 hover:text-orange-400 hover:cursor-default">
+              {profileOwner.username}
+            </div>
+            <div className="line-clamp-4 transition-colors duration-300 hover:text-orange-400 hover:cursor-default">
+              {profileOwner.description ??
+                `We don't know much about ${profileOwner.username}, but we are sure they are great!`}
+            </div>
+          </div>
+        </div>
+      </section>
+    )
   );
 }
