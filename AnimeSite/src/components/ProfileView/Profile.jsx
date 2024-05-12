bc9ed8db825ea5897cad589e04cac1899531acd2
udK01 <dominik.muranyi@gmail.com>
2024-05-12 17:03:34 +0100

    Reorganised Profile.

--- AnimeSite/src/components/ProfileView/Profile.jsx
+++ AnimeSite/src/components/ProfileView/Profile.jsx
@@ -54,145 +54,164 @@ export default function Profile() {
     return comics.find((manga) => manga.mangaID === id);
   }
 
-  return (
-    profileOwner && (
-      <>
-        {/* Basic Profile Info */}
-        <section className="w-[826px] h-auto rounded-sm font-poppins text-white">
-          {/* Route */}
-          <div className="flex justify-between text-[14px] w-full bg-quaternary rounded-sm text-white p-2">
-            <div className="flex">
-              <Link
-                to={"/"}
-                className="hover:text-primary mr-2 transition-colors duration-300"
-              >
-                Home
-              </Link>
-              <p className="transition-colors duration-300">&gt;</p>
-              <Link
-                to={`/profile?user=${profileOwner.username}`}
-                className="hover:text-primary mx-2 transition-colors duration-300"
-              >
-                {profileOwner.username}
-              </Link>
+  const DisplayComment = () => {
+    return (
+      <section className="w-[826px] h-auto rounded-sm font-poppins text-white">
+        <div className="p-4 bg-quaternary mt-10">
+          <div>Comments Posted</div>
+          <Separator />
+          {comments.length > 0 ? (
+            <div>
+              {comments.map((comment) => (
+                <React.Fragment key={comment.commentID}>
+                  <div className="border-l-2 border-primary">
+                    <div className="flex justify-between mx-2 mt-5">
+                      <div className="flex">
+                        <div className="text-orange-400">
+                          {profileOwner.username}
+                        </div>
+                        <div className="mx-[6px]">commented under</div>
+                        <Link
+                          to={`/${findManga(comment.mangaID).mangaTitle.replace(
+                            /\s+/g,
+                            "-"
+                          )}`}
+                          className="text-orange-400 hover:text-primary hover:cursor-pointer transition-colors duration-200"
+                        >
+                          {findManga(comment.mangaID).mangaTitle}
+                        </Link>
+                      </div>
+
+                      <div>
+                        {dateFormatter.getFormattedDate(comment.uploadDate)}
+                      </div>
+                    </div>
+                    <div className="text-dimWhite ml-2">{comment.content}</div>
+                  </div>
+                </React.Fragment>
+              ))}
             </div>
+          ) : (
+            <div className="w-full text-center text-[24px] font-bold my-4 hover:underline hover:text-primary hover:cursor-default">
+              This user has yet to comment anything. :c
+            </div>
+          )}
+        </div>
+      </section>
+    );
+  };
+
+  const DisplayBookmarks = () => {
+    return (
+      <section className="w-[826px] h-auto rounded-sm font-poppins text-white">
+        <div className="p-4 bg-quaternary mt-10">
+          <div>Top Bookmarks</div>
+          <Separator />
+          <div className="w-full flex flex-wrap">
+            {bookmarks.length > 0 ? (
+              bookmarks.slice(0, 5).map((bookmark, index) => (
+                <div key={index} className="flex ml-[11px]">
+                  <Card manga={findManga(bookmark.mangaID)} />
+                </div>
+              ))
+            ) : (
+              <div className="w-full text-center text-[24px] font-bold my-4 hover:underline hover:text-primary hover:cursor-default">
+                This user has yet to bookmark anything. :c
+              </div>
+            )}
           </div>
-          {/* Body */}
-          <div className="flex w-full h-auto bg-quaternary mt-10 p-4">
-            {/* Left */}
-            <div className="flex flex-col w-[180px] flex-shrink-0">
-              {/* Image Selector */}
-              <div className="relative border-2 size-36 border-primary flex justify-center items-center bg-secondary">
-                <img
-                  src={profileOwner.profilePicture}
-                  alt={profileOwner.profilePicture}
-                  className={`${editing && "brightness-50"} w-full h-full`}
-                />
-                {editing && (
-                  <div
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-secondary rounded-full p-1"
-                    onDragOver={handleDragOver}
-                    onDrop={handleDrop}
+        </div>
+      </section>
+    );
+  };
+
+  const DisplayUser = () => {
+    return (
+      <section className="w-[826px] h-auto rounded-sm font-poppins text-white">
+        {/* Route */}
+        <div className="flex justify-between text-[14px] w-full bg-quaternary rounded-sm text-white p-2">
+          <div className="flex">
+            <Link
+              to={"/"}
+              className="hover:text-primary mr-2 transition-colors duration-300"
+            >
+              Home
+            </Link>
+            <p className="transition-colors duration-300">&gt;</p>
+            <Link
+              to={`/profile?user=${profileOwner.username}`}
+              className="hover:text-primary mx-2 transition-colors duration-300"
+            >
+              {profileOwner.username}
+            </Link>
+          </div>
+        </div>
+        {/* Body */}
+        <div className="flex w-full h-auto bg-quaternary mt-10 p-4">
+          {/* Left */}
+          <div className="flex flex-col w-[180px] flex-shrink-0">
+            {/* Image Selector */}
+            <div className="relative border-2 size-36 border-primary flex justify-center items-center bg-secondary">
+              <img
+                src={profileOwner.profilePicture}
+                alt={profileOwner.profilePicture}
+                className={`${editing && "brightness-50"} w-full h-full`}
+              />
+              {editing && (
+                <div
+                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-secondary rounded-full p-1"
+                  onDragOver={handleDragOver}
+                  onDrop={handleDrop}
+                >
+                  <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className="hidden"
+                    id="fileInput"
+                  />
+                  <label
+                    htmlFor="fileInput"
+                    className="cursor-pointer text-white hover:text-primary text-3xl"
                   >
-                    <input
-                      type="file"
-                      accept="image/*"
-                      onChange={handleImageChange}
-                      className="hidden"
-                      id="fileInput"
-                    />
-                    <label
-                      htmlFor="fileInput"
-                      className="cursor-pointer text-white hover:text-primary text-3xl"
-                    >
-                      <FaEdit />
-                    </label>
-                    {image && (
-                      <p className="absolute w-[150px] -translate-x-12 text-white mt-2">
-                        Selected Image: <br /> {image.name}
-                      </p>
-                    )}
-                  </div>
-                )}
-              </div>
-            </div>
-            {/* Right */}
-            <div className="flex flex-col">
-              <div className=" ml-4 text-white text-[32px] w-full transition-colors duration-300 hover:text-orange-400 hover:cursor-default">
-                {profileOwner.username}
-              </div>
-              <div className="line-clamp-4 transition-colors duration-300 hover:text-orange-400 hover:cursor-default">
-                {profileOwner.description ??
-                  `We don't know much about ${profileOwner.username}, but we are sure they are great!`}
-              </div>
-            </div>
-          </div>
-        </section>
-        {/* Bookmarks */}
-        <section className="w-[826px] h-auto rounded-sm font-poppins text-white">
-          <div className="p-4 bg-quaternary mt-10">
-            <div>Top Bookmarks</div>
-            <Separator />
-            <div className="w-full flex flex-wrap">
-              {bookmarks.length > 0 ? (
-                bookmarks.slice(0, 5).map((bookmark) => (
-                  <div key={bookmark.bookmarkID} className="flex ml-[11px]">
-                    <Card manga={findManga(bookmark.mangaID)} />
-                  </div>
-                ))
-              ) : (
-                <div className="w-full text-center text-[24px] font-bold my-4 hover:underline hover:text-primary hover:cursor-default">
-                  This user has yet to bookmark anything. :c
+                    <FaEdit />
+                  </label>
+                  {image && (
+                    <p className="absolute w-[150px] -translate-x-12 text-white mt-2">
+                      Selected Image: <br /> {image.name}
+                    </p>
+                  )}
                 </div>
               )}
             </div>
           </div>
-        </section>
-        {/* Comments */}
-        <section className="w-[826px] h-auto rounded-sm font-poppins text-white">
-          <div className="p-4 bg-quaternary mt-10">
-            <div>Comments Posted</div>
-            <Separator />
-            {comments.length > 0 ? (
-              <div>
-                {comments.map((comment) => (
-                  <React.Fragment key={comment.commentID}>
-                    <div className="border-l-2 border-primary">
-                      <div className="flex justify-between mx-2 mt-5">
-                        <div className="flex">
-                          <div className="text-orange-400">
-                            {profileOwner.username}
-                          </div>
-                          <div className="mx-[6px]">commented under</div>
-                          <Link
-                            to={`/${findManga(
-                              comment.mangaID
-                            ).mangaTitle.replace(/\s+/g, "-")}`}
-                            className="text-orange-400 hover:text-primary hover:cursor-pointer transition-colors duration-200"
-                          >
-                            {findManga(comment.mangaID).mangaTitle}
-                          </Link>
-                        </div>
-
-                        <div>
-                          {dateFormatter.getFormattedDate(comment.uploadDate)}
-                        </div>
-                      </div>
-                      <div className="text-dimWhite ml-2">
-                        {comment.content}
-                      </div>
-                    </div>
-                  </React.Fragment>
-                ))}
-              </div>
-            ) : (
-              <div className="w-full text-center text-[24px] font-bold my-4 hover:underline hover:text-primary hover:cursor-default">
-                This user has yet to comment anything. :c
-              </div>
-            )}
+          {/* Right */}
+          <div className="flex flex-col">
+            <div className=" ml-4 text-white text-[32px] w-full transition-colors duration-300 hover:text-orange-400 hover:cursor-default">
+              {profileOwner.username}
+            </div>
+            <div className="line-clamp-4 transition-colors duration-300 hover:text-orange-400 hover:cursor-default">
+              {profileOwner.description ??
+                `We don't know much about ${profileOwner.username}, but we are sure they are great!`}
+            </div>
           </div>
-        </section>
-      </>
-    )
+        </div>
+      </section>
+    );
+  };
+
+  return (
+    <section className="w-[826px]">
+      {profileOwner && (
+        <>
+          {/* Basic Profile Info */}
+          <DisplayUser />
+          {/* Bookmarks */}
+          <DisplayBookmarks />
+          {/* Comments */}
+          <DisplayComment />
+        </>
+      )}
+    </section>
   );
 }
