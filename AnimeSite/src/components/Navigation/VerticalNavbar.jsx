72ed43f2b7fd0edb189dc8435ebc3a4e56a92de9
udK01 <dominik.muranyi@gmail.com>
2024-05-18 19:18:28 +0100

    Completed mobile nav bar.

--- AnimeSite/src/components/Navigation/VerticalNavbar.jsx
+++ AnimeSite/src/components/Navigation/VerticalNavbar.jsx
@@ -1,9 +1,57 @@
+import { useContext, useState } from "react";
+
+import { Link } from "react-router-dom";
+
 import { FiAlignJustify } from "react-icons/fi";
 
+import Separator from "../Separator";
+import UserContext from "../UserContext";
+import ComicsContext from "../ComicsProvider";
+
 export default function VerticalNavbar() {
+  const [show, setShow] = useState(false);
+  const { user } = useContext(UserContext);
+  const { comics } = useContext(ComicsContext);
+  const [path, setPath] = useState(
+    `/inspect?manga=${
+      comics[Math.floor(Math.random() * comics.length)].mangaID
+    }`
+  );
+
+  function toggleShow() {
+    setShow(!show);
+  }
+
+  function randomPath() {
+    setPath(
+      `/inspect?manga=${
+        comics[Math.floor(Math.random() * comics.length)].mangaID
+      }`
+    );
+  }
+
   return (
     <div>
-      <FiAlignJustify size={32} color="white" />
+      <FiAlignJustify size={32} color="white" onClick={() => toggleShow()} />
+      {show && (
+        <div className="w-[150px] h-auto flex flex-col space-y-1 p-2 items-center -translate-x-[98px] border-2 border-primary bg-secondary absolute z-20">
+          <Link to={"/"}>Home</Link>
+          <Separator />
+          <Link to={"/bookmarks"}>Bookmarks</Link>
+          <Separator />
+          <Link to={"/comics"}>Comics</Link>
+          <Separator />
+          <Link to={path} onClick={randomPath} className="text-orange-500">
+            Surprise Me
+          </Link>
+          {user.length > 0 && user[0].accessLevel > 0 && (
+            <>
+              <Separator />
+              <Link to={"/management"}>Management</Link>
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
