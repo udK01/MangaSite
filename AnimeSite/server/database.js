2f54e0b23189ca8a0d8afced32acae04aec089fe
udK01 <dominik.muranyi@gmail.com>
2024-05-27 15:37:45 +0100

    Fixed on local server.

--- AnimeSite/server/database.js
+++ AnimeSite/server/database.js
@@ -11,7 +11,6 @@ const db = mysql
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306,
   })
   .promise();
 
--- AnimeSite/vite.config.js
+++ AnimeSite/vite.config.js
@@ -1,20 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import dotenv from "dotenv";
-
-dotenv.config({ path: "./server/.env" });
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 5173,
     proxy: {
-      "/api": {
-        target: process.env.API,
-        changeOrigin: true,
-        secure: false,
-      },
+      "/api": "http://localhost:8080",
     },
   },
 });
