90dac6d2a329e6587d931163640ee438a2f957d0
udK01 <dominik.muranyi@gmail.com>
2024-06-02 14:01:28 +0100

    Made footer responsive.

--- AnimeSite/src/components/Footer.jsx
+++ AnimeSite/src/components/Footer.jsx
@@ -4,17 +4,17 @@ import { logo } from "../assets";
 export default function Footer() {
   return (
     <section className="flex flex-col w-full mt-10 text-white">
-      <div className="flex w-full h-[20px] justify-center items-center font-poppins bg-primary">
-        {/* {footer_text.map((footT, i) => (
+      <div className="flex w-full h-auto justify-center items-center font-poppins bg-primary">
+        {footer_text.map((footT, i) => (
           <p
             key={footT.id}
             className={`${
               i !== footT.length - 1 ? "ml-5" : ""
-            } hover:bg-secondary hover:cursor-pointer p-2 transition-all duration-300`}
+            } hover:bg-secondary hover:cursor-pointer p-2 transition-all duration-300 md:text-[16px] 2xs:text-[10px]`}
           >
             {footT.title}
           </p>
-        ))} */}
+        ))}
       </div>
       <div className="h-[100px] flex justify-center items-center bg-quaternary">
         <img src={logo} className="w-[50px] h-[50px] object-contain" />
